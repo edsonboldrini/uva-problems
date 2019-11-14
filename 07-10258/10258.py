d6bf5133bdac72b83a9be36fc36070edbed4171e
Edson Boldrini <edsonboldrini@gmail.com>
2019-11-14 10:47:04 -0200

    solution for problem 7 done

--- 07-10258/10258.py
+++ 07-10258/10258.py
@@ -1,9 +1,10 @@
 from operator import itemgetter
 
+
 def solve(submissions):
     correctSubmissions = {}
+    incorrectSubmissions = {}
     contestants = {}
-    submissions = sorted(submissions, key=itemgetter(3))
     for s in submissions:
         result = s[3]
         contestant = s[0]
@@ -12,32 +13,39 @@ def solve(submissions):
             if (result == "C"):
                 if(submissionData not in correctSubmissions):
                     correctSubmissions[submissionData] = s[2]
-                    if(contestant not in contestants):
-                        contestants[contestant] = [int(contestant), 1, int(s[2])]                        
+                    if(submissionData in incorrectSubmissions):
+                        if(contestant not in contestants):
+                            contestants[contestant] = [
+                                int(contestant), 1, int(s[2])+20]
+                        else:
+                            contestants[contestant] = [int(contestant), int(
+                                contestants[contestant][1])+1, int(contestants[contestant][2])+int(s[2])+20]
                     else:
-                        contestants[contestant] = [int(contestant), int(contestants[contestant][1])+1, int(contestants[contestant][2])+int(s[2])]
+                        if(contestant not in contestants):
+                            contestants[contestant] = [
+                                int(contestant), 1, int(s[2])]
+                        else:
+                            contestants[contestant] = [int(contestant), int(
+                                contestants[contestant][1])+1, int(contestants[contestant][2])+int(s[2])]
             else:
-                if(submissionData in correctSubmissions):    
-                    if(contestant not in contestants):
-                        contestants[contestant] = [int(contestant), 0, 20]
-                    else:
-                        contestants[contestant] = [int(contestant), int(contestants[contestant][1]), int(contestants[contestant][2])+20]
-                else:
-                    if(contestant not in contestants):
-                        contestants[contestant] = [int(contestant), 0, 0]
+                if(submissionData not in incorrectSubmissions):
+                    incorrectSubmissions[submissionData] = s[2]
+
+                if(contestant not in contestants):
+                    contestants[contestant] = [int(contestant), 0, 0]
         else:
             if(contestant not in contestants):
-                    contestants[contestant] = [int(contestant), 0, 0]
+                contestants[contestant] = [int(contestant), 0, 0]
 
-    # print(validSubmissions)
     contestants = [v for v in contestants.values()]
     contestants = sorted(contestants, key=itemgetter(0))
     contestants = sorted(contestants, key=itemgetter(2))
     contestants = sorted(contestants, key=itemgetter(1), reverse=True)
-    
+
     for c in contestants:
         print(c[0], c[1], c[2])
-    print("")
+    
+
 
 def main():
     try:
@@ -48,12 +56,14 @@ def main():
             submissions = []
             line = input()
             while(line != ""):
-                try:                
+                try:
                     submissions.append(line.split(' '))
                     line = input()
                 except EOFError:
-                    break
+                    break            
             solve(submissions)
+            if (i != numberOfTests -1):
+                print("")
     except EOFError:
         print("No lines")
 
