c0587706b780ec81c2f0005b04ab2dd3d27a8321
Edson Boldrini <edsonboldrini@gmail.com>
2019-11-19 22:03:51 -0300

    Problem 10 solved

---
+++ 10-11988/11988.py
@@ -0,0 +1,35 @@
+def solve(sentence):
+    resultSentence = ""
+    cursor = len(resultSentence)
+    for c in sentence:
+        if(c == "["):
+            cursor = 0
+        elif(c == "]"):
+            cursor = len(resultSentence)
+        else:
+            if (cursor == 0):
+                resultSentence = c + resultSentence
+            elif (cursor > 0 and cursor < len(resultSentence)):
+                resultSentence = resultSentence[:cursor] + c + resultSentence[cursor:]
+            elif (cursor == len(resultSentence)):
+                resultSentence = resultSentence + c
+            cursor += 1
+
+    print(resultSentence)
+
+
+def main():
+    try:
+        line = input()
+        while(line != ""):
+            try:
+                solve(line)
+                line = input()
+            except EOFError:
+                break
+    except EOFError:
+        print("No lines")
+
+
+if __name__ == "__main__":
+    main()
